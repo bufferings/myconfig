2a85368f6c5872f7fbd4e1db766f09b468bd3432
Mitsuyuki Shiiba <bufferings+github@gmail.com>
2018-02-26 17:00:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 ln -sf .vimrc ~/.vimrc
 ln -sf .tmux.conf ~/.tmux.conf
 
-dconf load /com/gexperts/Tilix/ < dconf/tilix.dconf
+dconf load /com/gexperts/Tilix/ < dconf/tilix.conf
 
 cp git/gitconfig /etc/gitconfig
