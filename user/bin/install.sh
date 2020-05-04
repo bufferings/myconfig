4b680bbf5d53108c991ab320cefbff2d00c24578
Mitsuyuki Shiiba <bufferings@gmail.com>
2020-05-04 10:49:16 +0900

    Add zsh configs

--- user/bin/install.sh
+++ user/bin/install.sh
@@ -5,5 +5,8 @@ BASE_DIR=$(cd $(dirname $0)/..; pwd)
 ln -sf "${BASE_DIR}/vim/.vimrc" ~/.vimrc
 ln -sf "${BASE_DIR}/tmux/.tmux.conf" ~/.tmux.conf
 
+ln -sf "${BASE_DIR}/zsh/.zpreztorc" ~/.zpreztorc
+ln -sf "${BASE_DIR}/zsh/.zshrc" ~/.zshrc
+
 # Tilix
 dconf load /com/gexperts/Tilix/ < ${BASE_DIR}/tilix/tilix.dconf
---
+++ user/zsh/.zshrc
@@ -0,0 +1,7 @@
+# I need to set up Prezto beforehand.
+
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
